0283c2441a2db4e1057d3bfed914d00b6ef908bf
kyungba <kkbfore@naver.com>
2020-09-14 14:07:25 +0900

    ing

--- js/script.js
+++ js/script.js
@@ -13,8 +13,8 @@ var script = (function(){
 					w = $(this).width();
 					h = $(this).height();
 					x = ( event.pageX - $(this).offset().left - ( w/2 )) * ( w > h ? ( h/w ) : 1 ),
-						y = ( event.pageY - $(this).offset().top  - ( h/2 )) * ( h > w ? ( w/h ) : 1 ),
-						direction = Math.round( ( ( ( Math.atan2(y, x) * (180 / Math.PI) ) + 180 ) / 90 ) + 3 )  % 4;
+					y = ( event.pageY - $(this).offset().top  - ( h/2 )) * ( h > w ? ( w/h ) : 1 ),
+					direction = Math.round( ( ( ( Math.atan2(y, x) * (180 / Math.PI) ) + 180 ) / 90 ) + 3 )  % 4;
 
 					$(this).find(cover).show();
 
@@ -35,25 +35,25 @@ var script = (function(){
 					w = $(this).width();
 					h = $(this).height();
 					x = ( event.pageX - $(this).offset().left - ( w/2 )) * ( w > h ? ( h/w ) : 1 ),
-						y = ( event.pageY - $(this).offset().top  - ( h/2 )) * ( h > w ? ( w/h ) : 1 ),
-						direction = Math.round( ( ( ( Math.atan2(y, x) * (180 / Math.PI) ) + 180 ) / 90 ) + 3 )  % 4;
+					y = ( event.pageY - $(this).offset().top  - ( h/2 )) * ( h > w ? ( w/h ) : 1 ),
+					direction = Math.round( ( ( ( Math.atan2(y, x) * (180 / Math.PI) ) + 180 ) / 90 ) + 3 )  % 4;
 
 					if(direction == 0) {
 						TweenMax.to($(this).find(cover), 0.4, {top:-h, left:0, ease:Power3.easeOut, onComplete:function () {
-								$(this).parent().find(cover).hide();
-							}});
+							$(this).parent().find(cover).hide();
+						}});
 					} else if(direction == 1) {
 						TweenMax.to($(this).find(cover), 0.4, {top:0, left:w, ease:Power3.easeOut, onComplete:function () {
-								$(this).parent().find(cover).hide();
-							}});
+							$(this).parent().find(cover).hide();
+						}});
 					} else if(direction == 2) {
 						TweenMax.to($(this).find(cover), 0.4, {top:h, left:0, ease:Power3.easeOut, onComplete:function () {
-								$(this).parent().find(cover).hide();
-							}});
+							$(this).parent().find(cover).hide();
+						}});
 					} else {
 						TweenMax.to($(this).find(cover), 0.4, {top:0, left:-w, ease:Power3.easeOut, onComplete:function () {
-								$(this).parent().find(cover).hide();
-							}});
+							$(this).parent().find(cover).hide();
+						}});
 					}
 				})
             })
@@ -87,16 +87,29 @@ var script = (function(){
 					if(!$(".viewPop").hasClass($(this).attr("id"))){
 						$(".viewPop").removeClass().addClass("viewPop").addClass($(this).attr("id"));
 						$(".viewPop .titArea .tit span").text($(this).find(".txtArea .tit").text());
-						if($.trim($(this).data("href")) == ""){
-							$(".viewPop .titArea .link").hide();
-						}else{
+						if(!$(this).hasClass("etc")){
 							$(".viewPop .titArea .link").show();
-							$(".viewPop .titArea .link").attr("href", $(this).data("href"));
-						}
-						$(".viewPop .conArea .imgArea").empty();
-						$(".viewPop").height("");
-						for (var i=0; i < $(this).data("img-size"); i++){
-							$(".viewPop .conArea .imgArea").append('<img src="./images/project_view_'+$(this).attr("id")+i+'.jpg" alt="" />');
+							if($(this).data("href") == "intra"){
+								if(!$(".viewPop .titArea .link").hasClass("on")){
+									$(".viewPop .titArea .link").addClass("on");
+								}
+							}else{
+								if($(".viewPop .titArea .link").hasClass("on")){
+									$(".viewPop .titArea .link").removeClass("on");
+								}
+								$(".viewPop .titArea .link").attr("href", $(this).data("href"));
+							}
+							$(".viewPop .etcArea").empty();
+							$(".viewPop .conArea .imgArea").empty();
+							$(".viewPop").height("");
+							for (var i=0; i < $(this).data("img-size"); i++){
+								$(".viewPop .conArea .imgArea").append('<img src="./images/project_view_'+$(this).attr("id")+i+'.jpg" alt="" />');
+							}
+						}else{
+							$(".viewPop .titArea .link").hide();
+							$(".viewPop .conArea .imgArea").empty();
+							$(".viewPop").height("");
+							$(".viewPop .conArea .etcArea").html($(this).find(".kindD").html());
 						}
 						if($.trim($(this).data("img-size")) != ""){
 							if(parseInt(_winH * 0.9) % 2 == 1){
